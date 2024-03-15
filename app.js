0402db1494ac0c08e35222d608ca7964c9a74ff8
Uma Chandra Sekhara Sarma <chandu06p@gmail.com>
2024-03-15 16:40:30 +0530

    update

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import  express  from "express";
+import cors from 'cors';
 import UseRouter from "./routes/User/UserRoutes.js";
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(UseRouter)
 export default app
