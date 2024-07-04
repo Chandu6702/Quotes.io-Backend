b898e0edfaa9901a8d213c3883a3fa97ef481dc3
Uma Chandra Sekhara Sarma Thadepalli <chandrasekhara067@gmail.com>
2024-07-04 15:03:34 +0530

    update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import app from './app.js'
-import dbConnect from './database/dbConnect.js'
+import app from "./app.js";
+import dbConnect from "./database/dbConnect.js";
 
-
-
-dbConnect().then(() => {
+dbConnect()
+  .then(() => {
     app.listen(3000, () => {
-        console.log('Server is running');
-    })
-}).catch((error) => {
+      console.log("Server is running");
+    });
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
